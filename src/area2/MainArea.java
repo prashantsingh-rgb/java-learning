dac93a0828bd38fa975c790ba4c2be01933c072c
Prashant <prashantkumar532004@gmail.com>
2026-02-11 16:04:48 +0530

    implementation dn abstraction

--- src/area2/MainArea.java
+++ src/area2/MainArea.java
@@ -3,9 +3,28 @@ package area2;
 abstract class Area{
     abstract void getArea();
 }
+
+class Rectangle extends Area{
+
+    int l,b;
+    Rectangle(int l,int b){
+        this.l=l;
+        this.b=b;
+    }
+void getArea(){
+ int r=l*b;
+    System.out.println("Area of Rectangle ="+r);
+
+}
+
+void displayResult(){
+    System.out.println("This is Display Function in rectangle");
+}
+}
 public class MainArea {
 
     public static void main(String[] args) {
-
+      Rectangle r1 = new Rectangle(24,26);
+      r1.getArea();
     }
 }
---
+++ src/poly/MainPoly.java
@@ -0,0 +1,14 @@
+package poly;
+
+class Animal{
+    String name;
+    String sound;
+    void makeSound(){
+        System.out.println("This is Animal Class");
+    }
+}
+class Cat extends Animal{
+
+}
+public class MainPoly {
+}
