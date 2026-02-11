0d0644d5f9e3d56913fd50a7c213e9fc0a6cb690
Prashant <prashantkumar532004@gmail.com>
2026-02-11 16:16:50 +0530

    implementation dn abstraction

--- src/poly/MainPoly.java
+++ src/poly/MainPoly.java
@@ -6,9 +6,20 @@ class Animal{
     void makeSound(){
         System.out.println("This is Animal Class");
     }
+    void display(){
+        String result = String.format("Name=%s and Sound =%s",name,sound);
+        System.out.println(result);
+    }
+
 }
 class Cat extends Animal{
-
+    int legs;
+    Cat(String name,String sound, int legs){
+        super.name=name;
+        super.sound=sound;
+        this.legs=legs;
+    }
+   void makeSound()
 }
 public class MainPoly {
 }
